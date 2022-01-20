cc98101233cf87364a95e1895e62ba222bdeee3e
Autumn <arogers@westridge.org>
2022-01-20 14:10:11 -0800

    Better formatting for request/response

--- theinternetproject/http_clientserver.js
+++ theinternetproject/http_clientserver.js
@@ -2,13 +2,16 @@
 const server_data = {
     "theinternet.com": '<!DOCTYPE html><html><head><title>The Internet</title><link rel="stylesheet" href="theinternet.css"></head><body><div id="box1"><h1>Welcome to the Internet</h1><p>Can you use JavaScript to modify this page?</p><p class="invisible">Seeeeecret text</p></div><div class="invisible"><h1>This is also secret text</h1><h6>This is technically a header but it is very small</h6></div></body></html>'
 }
+// The client's IP address
+const client_ip = "49.81.27.109";
 
 /* HTTP is to get HTML from the server */
 function clientHTTP(url) {
     // Build HTTP request
     let http_request = {
-        "url": url,
-        "request_method": "GET"
+        "src": client_ip,           // The source (the client)
+        "dest": url,                // The destination (the server)
+        "request_method": "GET"     // Request type (get content)
     }
     
     // Send this to the server (pass HTTP request as an argument)
@@ -17,14 +20,16 @@ function clientHTTP(url) {
     return response.content;
 }
 
+/* Server receives the request and returns a response with the HTML */
 function serverHTTP(http_request) {
     // Get the HTML data
-    let html = server_data[http_request.url];
+    let html = server_data[http_request.dest];
 
     // Build the response with the HTML
     let http_response = {
-        "dest_ip": http_request.src_ip, // Where this is going -- client IP
-        "content": html,                // The HTML for this site
+        "src": http_request.dest,    // The source (this website)
+        "dest": http_request.src,    // Where this is going (client)
+        "content": html,             // The HTML for this site
     }
     
     // Send back to the client
--- theinternetproject/http_clientserver_witherrorhandling.js
+++ theinternetproject/http_clientserver_witherrorhandling.js
@@ -2,13 +2,16 @@
 const server_data = {
     "theinternet.com": '<!DOCTYPE html><html><head><title>The Internet</title><link rel="stylesheet" href="theinternet.css"></head><body><div id="box1"><h1>Welcome to the Internet</h1><p>Can you use JavaScript to modify this page?</p><p class="invisible">Seeeeecret text</p></div><div class="invisible"><h1>This is also secret text</h1><h6>This is technically a header but it is very small</h6></div></body></html>'
 }
+// The client's IP address
+const client_ip = "49.81.27.109";
 
 /* HTTP is to get HTML from the server */
 function clientHTTP(url) {
     // Build HTTP request
     let http_request = {
-        "url": url,
-        "request_method": "GET"
+        "src": client_ip,           // The source (the client)
+        "dest": url,                // The destination (the server)
+        "request_method": "GET"     // Request type (get content)
     }
     
     // Send this to the server (pass HTTP request as an argument)
@@ -17,29 +20,28 @@ function clientHTTP(url) {
     return response.content;
 }
 
+/* Server receives the request and returns a response with the HTML */
 function serverHTTP(http_request) {
     // Define this so we have it available at the end of the function
-    let http_response;
+    // src and dest will be the same whether this is a valid request or not
+    let http_response = {
+        "src": http_request.dest,    // The source (this website)
+        "dest": http_request.src,    // Where this is going (client)
+    }
 
     /* We only support GET requests for defined URLs. 
        If this isn't GET, return 400
        If the URL isn't in the server data, return 400
      */
-    if (http_request.request_method == "GET" && Object.keys(server_data).indexOf(http_request.url) > -1) {
-        // Get the HTML for the requested URL
-        let html = server_data[http_request.url];
-
-        // Build the response with the HTML
-        http_response = {
-            "dest_ip": http_request.src_ip, // Where this is going -- client IP
-            "content": html,                // The HTML for this site
-        }
+    if (http_request.request_method == "GET" && Object.keys(server_data).indexOf(http_request.dest) > -1) {
+        // Get the HTML data
+        let html = server_data[http_request.dest];
+        // Add the HTML to the response
+        http_response.content = html;
     }
     else {
-        http_response = {
-            "dest_ip": http_request.src_ip, // Where this is going -- client IP
-            "content": "400",               // Bad request -- 400
-        }
+        // Bad request
+        http_response.content = "400";
     }
     
     // Send back to the client
