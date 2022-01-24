a0537c67eb6ae4ef990393b2b3ee9a62e4c0d90d
Autumn <arogers@westridge.org>
2022-01-24 13:32:57 -0800

    Remove client IP (unnecessary for this file)

--- theinternetproject/02_http_clientserver.js
+++ theinternetproject/02_http_clientserver.js
@@ -2,8 +2,6 @@
 const server_data = {
     "theinternet.com": '<!DOCTYPE html><html><head><title>The Internet</title><link rel="stylesheet" href="theinternet.css"></head><body><div id="box1"><h1>Welcome to the Internet</h1><p>Can you use JavaScript to modify this page?</p><p class="invisible">Seeeeecret text</p></div><div class="invisible"><h1>This is also secret text</h1><h6>This is technically a header but it is very small</h6></div></body></html>'
 }
-// The client's IP address
-const client_ip = "49.81.27.109";
 
 /* HTTP is to get HTML from the server */
 function clientHTTP(url) {
