7a160756a3f9ae2983502d048188d5793831645f
Autumn <arogers@westridge.org>
2022-02-16 14:55:40 -0800

    Bus address, two args

--- server_node_arduino/server_node_arduino.js
+++ server_node_arduino/server_node_arduino.js
@@ -8,22 +8,58 @@ var oscPort = new osc.UDPPort({
 
 oscPort.open();
 
+var freq = 0;
+
 oscPort.on("ready", function () {
     oscPort.send({
-        address: "/s_new",
+        address: "/bus",
         args: [
             {
-                type: "s",
-                value: "hello world"
+                type: "i",
+                value: "0"
             },
             {
                 type: "i",
-                value: 6969
+                value: "1"
             }
         ]
     }, "127.0.0.1", 6969);
-});
 
-setTimeout(() => {
-    oscPort.close();
-}, 100);
+    var inter = setInterval(() => {
+        oscPort.send({
+            address: "/bus",
+            args: [
+                {
+                    type: "i",
+                    value: "1"
+                },
+                {
+                    type: "f",
+                    value: freq
+                }
+            ]
+        }, "127.0.0.1", 6969);
+        freq += 0.1;
+        console.log(freq);
+
+        if (freq >= 1) {
+            oscPort.send({
+                address: "/bus",
+                args: [
+                    {
+                        type: "i",
+                        value: "0"
+                    },
+                    {
+                        type: "i",
+                        value: "0"
+                    }
+                ]
+            }, "127.0.0.1", 6969);
+            setTimeout(() => {
+                oscPort.close();
+            }, 200);
+            clearInterval(inter);       
+        }
+    }, 200)
+});
